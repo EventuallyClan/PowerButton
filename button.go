62048eb54cf7c20a18962be508baf326551619e2
Mary Larrousse <larroussemary@gmail.com>
2020-04-13 14:46:17 -0700

    targetconfig

--- button.go
+++ button.go
@@ -10,14 +10,16 @@ import (
 func onHandler(w http.ResponseWriter, r *http.Request) {
 	log.Print("Power button on request.")
 	//target := os.Getenv("TARGET")
-	resp, err := http.NewRequest("POST", "https://compute.googleapis.com/compute/v1/projects/silent-space-421/zones/us-central1-a/instances/3409664995533518179/start", nil)
+	resp, err := http.NewRequest("POST",
+		fmt.Sprintf("https://compute.googleapis.com/compute/v1/projects/silent-space-421/zones/us-central1-a/instances/%s/start", target), nil)
 	log.Print(resp, err)
 }
 
 func offHandler(w http.ResponseWriter, r *http.Request) {
 	log.Print("Power button off request.")
-	//target := os.Getenv("TARGET")
-	resp, err := http.NewRequest("POST", "https://compute.googleapis.com/compute/v1/projects/silent-space-421/zones/us-central1-a/instances/3409664995533518179/stop", nil)
+	target := os.Getenv("TARGET")
+	resp, err := http.NewRequest("POST",
+		fmt.Sprintf("https://compute.googleapis.com/compute/v1/projects/silent-space-421/zones/us-central1-a/instances/%s/stop", target), nil)
 	log.Print(resp, err)
 }
 
