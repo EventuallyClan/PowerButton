a0a80238ac46c9ea5c2c6053ec14825c2a750696
Mary Larrousse <larroussemary@gmail.com>
2020-04-13 12:44:05 -0700

    get good later

--- button.go
+++ button.go
@@ -9,14 +9,14 @@ import (
 
 func onHandler(w http.ResponseWriter, r *http.Request) {
 	log.Print("Power button on request.")
-	target := os.Getenv("TARGET")
+	//target := os.Getenv("TARGET")
 	resp, err := http.Post("https://compute.googleapis.com/compute/v1/projects/silent-space-421/zones/us-central1-a/instances/3409664995533518179/start")
 	log.Print(resp, err)
 }
 
 func offHandler(w http.ResponseWriter, r *http.Request) {
 	log.Print("Power button off request.")
-	target := os.Getenv("TARGET")
+	//target := os.Getenv("TARGET")
 	resp, err := http.Post("https://compute.googleapis.com/compute/v1/projects/silent-space-421/zones/us-central1-a/instances/3409664995533518179/stop")
 	log.Print(resp, err)
 }
