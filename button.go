306e17531b97fa675d539694dd4611074171f216
Mary Larrousse <larroussemary@gmail.com>
2020-04-15 17:20:38 -0700

    add status page

--- button.go
+++ button.go
@@ -5,58 +5,74 @@ import (
 	"log"
 	"net/http"
 	"os"
-	
+
 	"golang.org/x/net/context"
-        "golang.org/x/oauth2/google"
-        "google.golang.org/api/compute/v1"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/compute/v1"
 )
+
+var project string = os.Getenv("PROJECT")
+var zone string = os.Getenv("ZONE")
+var instance string = os.Getenv("INSTANCE")
 
 func onHandler(w http.ResponseWriter, r *http.Request) {
 	ctx := context.Background()
 
-        c, err := google.DefaultClient(ctx, compute.CloudPlatformScope)
-        if err != nil {
-                log.Fatal(err)
-        }
+	c, err := google.DefaultClient(ctx, compute.CloudPlatformScope)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-        computeService, err := compute.New(c)
-        if err != nil {
-                log.Fatal(err)
-        }
+	computeService, err := compute.New(c)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	project := os.Getenv("PROJECT")
-	zone := os.Getenv("ZONE")
-	instance := os.Getenv("INSTANCE")
-
-        resp, err := computeService.Instances.Start(project, zone, instance).Context(ctx).Do()
-        if err != nil {
-                log.Fatal(err)
-        }
+	resp, err := computeService.Instances.Start(project, zone, instance).Context(ctx).Do()
+	if err != nil {
+		log.Fatal(err)
+	}
 	log.Print(resp, resp.Status)
 }
 
 func offHandler(w http.ResponseWriter, r *http.Request) {
-        ctx := context.Background()
+	ctx := context.Background()
 
-        c, err := google.DefaultClient(ctx, compute.CloudPlatformScope)
-        if err != nil {
-                log.Fatal(err)
-        }
+	c, err := google.DefaultClient(ctx, compute.CloudPlatformScope)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-        computeService, err := compute.New(c)
-        if err != nil {
-                log.Fatal(err)
-        }
-	
-	project := os.Getenv("PROJECT")
-	zone := os.Getenv("ZONE")
-	instance := os.Getenv("INSTANCE")
+	computeService, err := compute.New(c)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-        resp, err := computeService.Instances.Stop(project, zone, instance).Context(ctx).Do()
-        if err != nil {
-                log.Fatal(err)
-        }
+	resp, err := computeService.Instances.Stop(project, zone, instance).Context(ctx).Do()
+	if err != nil {
+		log.Fatal(err)
+	}
 	log.Print(resp, resp.Status)
+}
+
+func statusHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := context.Background()
+
+	c, err := google.DefaultClient(ctx, compute.CloudPlatformScope)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	computeService, err := compute.New(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resp, err := computeService.Instances.Status(project, zone, instance).Context(ctx).Do()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Fprintf(resp, resp.Status)
 }
 
 func main() {
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module powerbutton-go
 
 go 1.13
+
+require (
+	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e // indirect
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
+	google.golang.org/api v0.21.0 // indirect
+)
