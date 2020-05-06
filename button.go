0a387ffda5ca514e3206f0eb1b3e08063dba2ba0
Mary Larrousse <larroussemary@gmail.com>
2020-05-06 11:55:14 -0700

    v2 Post Handlers

--- button.go
+++ button.go
@@ -15,8 +15,9 @@ var project string = os.Getenv("PROJECT")
 var zone string = os.Getenv("ZONE")
 var instance string = os.Getenv("INSTANCE")
 
+var ctx context.Context = context.Background()
+
 func onHandler(w http.ResponseWriter, r *http.Request) {
-	ctx := context.Background()
 
 	c, err := google.DefaultClient(ctx, compute.CloudPlatformScope)
 	if err != nil {
@@ -36,7 +37,6 @@ func onHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func offHandler(w http.ResponseWriter, r *http.Request) {
-	ctx := context.Background()
 
 	c, err := google.DefaultClient(ctx, compute.CloudPlatformScope)
 	if err != nil {
@@ -56,8 +56,6 @@ func offHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func statusHandler(w http.ResponseWriter, r *http.Request) {
-	ctx := context.Background()
-
 	c, err := google.DefaultClient(ctx, compute.CloudPlatformScope)
 	if err != nil {
 		log.Fatal(err)
@@ -75,12 +73,79 @@ func statusHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "%s, %s", resp.Status, resp.StatusMessage)
 }
 
+func v2onHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		w.WriteHeader(404)
+		fmt.Fprintf(w, "GET unsupported on v2")
+		return
+	}
+
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+
+	instanceName := r.FormValue("instanceName")
+
+	c, err := google.DefaultClient(ctx, compute.CloudPlatformScope)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	computeService, err := compute.New(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resp, err := computeService.Instances.Start(project, zone, instanceName).Context(ctx).Do()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Print(resp, resp.Status)
+	fmt.Fprintf(w, "%s, %s", resp.Status, resp.StatusMessage)
+}
+
+func v2offHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		w.WriteHeader(404)
+		fmt.Fprintf(w, "GET unsupported on v2")
+		return
+	}
+
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+
+	instanceName := r.FormValue("instanceName")
+
+	c, err := google.DefaultClient(ctx, compute.CloudPlatformScope)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	computeService, err := compute.New(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resp, err := computeService.Instances.Stop(project, zone, instanceName).Context(ctx).Do()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Print(resp, resp.Status)
+	fmt.Fprintf(w, "%s, %s", resp.Status, resp.StatusMessage)
+}
+
 func main() {
 	log.Print("Power Button Pushed")
 
 	http.HandleFunc("/on", onHandler)
 	http.HandleFunc("/off", offHandler)
 	http.HandleFunc("/status", statusHandler)
+
+	http.HandleFunc("v2/on", v2onHandler)
+	http.HandleFunc("v2/off", v2offHandler)
 
 	port := os.Getenv("PORT")
 	if port == "" {
